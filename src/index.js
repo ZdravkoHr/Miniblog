be0e63123540b388789c8d721a8c62bede5783f6
Zdravko Hristov <zdravkohristov0@gmail.com>
2022-09-09 18:52:25 +0300

    remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import './index.css';
 import App from './App';
 //import { BrowserRouter as Router, Route } from 'react-router-dom';
 
