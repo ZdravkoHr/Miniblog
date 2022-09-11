8683a084a9e00f117e3041c2a8c1c3ea66e78724
Zdravko Hristov <zdravkohristov0@gmail.com>
2022-09-11 13:44:49 +0300

    make refreshing of a blog work

--- src/components/Blogs/SingleBlog/SingleBlog.js
+++ src/components/Blogs/SingleBlog/SingleBlog.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useSelector } from 'react-redux';
 import { blogsSelector, userSelector } from '../../../store/store';
@@ -6,35 +7,44 @@ import SingleBlogEl from './SingleBlog.style';
 
 const SingleBlog = () => {
 	const { id } = useParams();
-	const { uid: userID } = useSelector(userSelector);
+	const [loaded, setLoaded] = useState(false);
 
-	const blog = useSelector(blogsSelector).blogs.filter(
+	const { uid: userID } = useSelector(userSelector) || {};
+
+	const blog = useSelector(blogsSelector)?.blogs.filter(
 		blog => blog.id === id
 	)[0];
 	const { title, subtitle, authorName, thumbnail, content } = blog || {};
 
+	useEffect(() => {
+		setLoaded(!!userID && !!blog);
+	}, [userID, blog]);
+
 	return (
-		<section className='single-blog-wrapper container'>
-			<SingleBlogEl>
-				<div className='top-info'>
-					<div className='title-row'>
-						<h2 className='title'>{title}</h2>
-						{console.log(userID, blog, userID === blog.authorID)}
-						{userID === blog.authorID && <EditButtons />}
-					</div>
-					<h4 className='subtitle'>{subtitle}</h4>
-					<h6 className='author'>Published by: {authorName}</h6>
-				</div>
+		<>
+			{loaded && (
+				<section className='single-blog-wrapper container'>
+					<SingleBlogEl>
+						<div className='top-info'>
+							<div className='title-row'>
+								<h2 className='title'>{title}</h2>
+								{userID && userID === blog.authorID && <EditButtons />}
+							</div>
+							<h4 className='subtitle'>{subtitle}</h4>
+							<h6 className='author'>Published by: {authorName}</h6>
+						</div>
 
-				<div className='thumbnail'>
-					<img src={thumbnail} alt={title} />
-				</div>
+						<div className='thumbnail'>
+							<img src={thumbnail} alt={title} />
+						</div>
 
-				<div className='content'>
-					<pre>{content}</pre>
-				</div>
-			</SingleBlogEl>
-		</section>
+						<div className='content'>
+							<pre>{content}</pre>
+						</div>
+					</SingleBlogEl>
+				</section>
+			)}
+		</>
 	);
 };
 
